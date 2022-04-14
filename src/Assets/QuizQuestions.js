9cdc748598ecfa186386bd016054903c827c3fa4
XiaoweiJiang <jiangxiaowei901@gmail.com>
2022-04-14 16:39:33 -0400

    quiz question added

--- src/Assets/QuizQuestions.js
+++ src/Assets/QuizQuestions.js
@@ -20,6 +20,28 @@ const questions=[
         {Answer: "Stabalize" , isTrue: true},
         {Answer: "Speed" , isTrue: false}
     ]
-}
+},
+{
+    QNumber: 2,
+    Question: "  Question #3: _________ is a good aerodynamic fin and is usually used on low-drag, high-performance rockets.",
+    Answer: 1,
+    Options: [
+        {Answer: "Trapezoidal" , isTrue: false},
+        {Answer: "Clipped Delta" , isTrue: true},
+        {Answer: "Elliptical" , isTrue: false},
+        {Answer: "Swept" , isTrue: false}
+    ]
+},
+{
+    QNumber: 3,
+    Question: "  Question #4: The larger the fins, the more wind is forced on the rockets and the ____ the rocket will rotate ____ the wind",
+    Answer: 0,
+    Options: [
+        {Answer: "faster , into" , isTrue: true},
+        {Answer: "faster , away from" , isTrue: false},
+        {Answer: "slower , into" , isTrue: false},
+        {Answer: "slower , away from" , isTrue: false}
+    ]
+},
 ]
 export default questions;
